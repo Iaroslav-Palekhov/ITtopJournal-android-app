ccf999a75903eb79df72c237581d611e2addb195
Iaroslav-Palekhov <palekhov2008@gmail.com>
2025-09-16 10:21:17 +0500

    Update MarketService.kt

--- app/src/main/java/ru/termux/topacademy/api/MarketService.kt
+++ app/src/main/java/ru/termux/topacademy/api/MarketService.kt
@@ -2,8 +2,11 @@ package ru.termux.topacademy.api
 
 import ru.termux.topacademy.model.MarketItem
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
+import ru.termux.topacademy.PurchaseRequest
 
 interface MarketService {
     @GET("market/customer/product/list")
@@ -11,6 +14,10 @@ interface MarketService {
         @Query("page") page: Int = 1,
         @Query("type") type: Int = 0
     ): Response<MarketResponse>
+
+    // Новый метод для покупки товара
+    @POST("market/customer/order/create")
+    suspend fun purchaseProduct(@Body request: PurchaseRequest): Response<Any>
 }
 
 data class MarketResponse(
